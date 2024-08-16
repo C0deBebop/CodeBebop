a4c5d2bf1fffa7cf55b164c5b6856a0100784df0
Rocky Latchman <kidmongoose@gmail.com>
2024-08-15 21:35:45 -0700

    Added blog template styles and HTML

--- app.rb
+++ app.rb
@@ -7,5 +7,26 @@ class CodeBebop < Sinatra::Base
     erb :index
   end  
 
+  post '/contact' do
+  
+  end  
+
+  get '/resume' do
+  
+  end 
+  
+  get '/blog' do
+    erb :blog
+  end
+  
+  get '/work' do
+
+  end
+  
+  
+
+
+
+
   run!
 end
